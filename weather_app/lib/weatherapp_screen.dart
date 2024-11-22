107af6ae48be5a96bce821523266655d006a9d74
Oluwasegun Osibajo <osibajooluwasegun051@gmail.com>
2024-11-22 15:48:26 +0100

    update weather App

--- weather_app/lib/weatherapp_screen.dart
+++ weather_app/lib/weatherapp_screen.dart
@@ -1,4 +1,6 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
+
 
 class WeatherScreen extends StatelessWidget {
   const WeatherScreen({super.key});
@@ -20,29 +22,111 @@ class WeatherScreen extends StatelessWidget {
           ),
       ],
       ),
-      body: const  Column(
-        children: [
-            // maincard
-             Card( 
-              child: Column(
+      body:  Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+              // maincard
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16)
+                  ),
+                elevation: 10, 
+                child:   ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                    child: const Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          Text('300 °F', style: 
+                            TextStyle(
+                              fontSize: 32,
+                              fontWeight:FontWeight.bold 
+                            ),
+                          ),
+                          SizedBox(height: 16,),
+                          Icon(Icons.cloud,
+                            size: 64,),
+                          SizedBox(height: 16,),
+                          Text('Rain', 
+                            style: TextStyle(
+                            fontSize: 20),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+           const  SizedBox(height: 20),
+           const Text('Weather Forecast', 
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            const SizedBox(height: 16),
+              //  weatherforcast Hieghts
+            const SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+              child:  Row(
                 children: [
-                  Text('300 °F', style: TextStyle(
-                    fontSize: 50,
-                  ),)
-                ],)
-             ),
-             SizedBox(height: 20),
-            //  weatherforcast Hieghts
-            Placeholder(
+                  HourlyForecastItem(),
+                  HourlyForecastItem(),
+                  HourlyForecastItem(),
+                  HourlyForecastItem(),
+                  HourlyForecastItem()
+                ],
+              ),
+            ),
+            const  Placeholder(
               fallbackHeight: 150,
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             // Additional information
-            Placeholder(
+            const Placeholder(
               fallbackHeight: 150,
             )
+        
+          ],
+        ),
+      ),
+    );
+  }
+}
+class HourlyForecastItem extends StatelessWidget {
+  const HourlyForecastItem({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      child:  Container(
+      width: 100,
+      padding: const EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(32)
+      ),
+      child: const  Column(
+        children: [
+          Text('03:00', 
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+          SizedBox(height: 8),
+          Icon(Icons.cloud, size: 32,),
+          SizedBox(height: 8),
+          Text('320 °F'),
         ],
+      ),
       ),
     );
   }
